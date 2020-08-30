c24e838c2322b05c3079eaa7bfa6d65e3cdbb684
Gareth Francis <gfrancis.dev@gmail.com>
2020-08-30 13:03:22 +0100

    Ctrl click to spawn cubey

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,9 @@
 extern crate kiss3d;
 extern crate nalgebra as na;
-use na::{Point3, Vector3};
+use na::{Point2, Point3, Vector3};
 
 use ncollide3d::shape::{Cuboid, ShapeHandle};
+use ncollide3d::query::{Ray, RayCast};
 use nphysics3d::force_generator::DefaultForceGeneratorSet;
 use nphysics3d::joint::DefaultJointConstraintSet;
 use nphysics3d::object::{
@@ -13,8 +14,10 @@ use nphysics3d::world::{DefaultGeometricalWorld, DefaultMechanicalWorld};
 use kiss3d::light::Light;
 use kiss3d::scene::SceneNode;
 use kiss3d::window::{Window};
-use kiss3d::camera::ArcBall;
-use kiss3d::event::{Action, Key, WindowEvent};
+use kiss3d::camera::{Camera,ArcBall};
+use kiss3d::planar_camera::*;
+use kiss3d::event::{Action, Key, MouseButton, WindowEvent, Modifiers};
+use kiss3d::text::Font;
 
 use std::collections::HashMap;
 use std::time::Instant;
@@ -144,14 +147,15 @@ fn main() {
     // Ground definition
     let ground_thickness = 0.2;
     let ground_width = 100.0;
-    let ground_shape = ShapeHandle::new(Cuboid::new(
+    let ground_collision_cuboid = Cuboid::new(
         Vector3::new(ground_width, ground_thickness, ground_width)
-    ));
+    );
+    let ground_shape = ShapeHandle::new(ground_collision_cuboid);
     let ground_handle = state.bodies.insert(Ground::new());
-    let co = ColliderDesc::new(ground_shape)
+    let ground_collider = ColliderDesc::new(ground_shape)
         .translation(Vector3::y() * -ground_thickness)
         .build(BodyPartHandle(ground_handle, 0));
-    state.colliders.insert(co);
+    state.colliders.insert(ground_collider);
 
     let mut ground_geometry = state.window.add_cube(ground_width, ground_thickness, ground_width);
     ground_geometry.set_color(0.9, 0.9, 0.9);
@@ -173,7 +177,10 @@ fn main() {
     // - We can't replace the camera on the window at all, there's no method to do it whatsoever
     // window.render_loop(state);
     let mut camera = ArcBall::new(Point3::new(5.0, 5.0, 5.0), Point3::new(0.0, 1.5, 0.0));
+    let mut planar_camera = FixedView::new();
     
+    let mut cursor_ray = Ray::new(na::Point3::new(0.0,0.0,0.0), na::Vector3::new(0.0,0.0,0.0));
+    let mut cursor_position = na::Point2::<f32>::new(0.0,0.0);
     while !state.window.should_close() {
         for event in state.window.events().iter() {
             match event.value {
@@ -182,11 +189,37 @@ fn main() {
                         add_primitive(&mut state, &primitives, "cubey", &Vector3::new(0.0, 50.0, 0.0));
                     }
                 },
+                WindowEvent::MouseButton(k, Action::Press, modif) => {
+                    if modif.contains(Modifiers::Control) {
+                        if k == MouseButton::Button2 {
+                            // Find the intersection between cursor ray and ground, then spawn something
+                            let toi = ground_collision_cuboid.toi_with_ray(&na::Isometry3::identity(), &cursor_ray, 10000.0, true).unwrap();
+                            if toi > 0.0 {
+                                let intersection_point = cursor_ray.origin + cursor_ray.dir * toi;
+                                add_primitive(&mut state, &primitives, "cubey", &Vector3::new(
+                                    intersection_point.x,
+                                    intersection_point.y + 20.0,
+                                    intersection_point.z,
+                                ));
+                            }
+                        }
+                    }
+                },
+                WindowEvent::CursorPos(x, y, _modif) => {
+                    let window_size = na::Vector2::new(state.window.size()[0] as f32, state.window.size()[1] as f32);
+                    let cursor_position_projected = na::Point2::new(x as f32, y as f32);
+
+                    // (position, direction)
+                    let ray = camera.unproject(&cursor_position_projected, &window_size);
+                    cursor_ray = Ray::new(ray.0, ray.1);
+                    
+                    // TODO: HAX! - abusing the planar camera to work out the cursor coords
+                    let unprojected = planar_camera.unproject(&cursor_position_projected, &window_size);
+                    cursor_position = unprojected;
+                }
                 _ => {}
             }
         }
-
-
 
         let simulation_elapsed_ms : f32 = state.simulation_start_time.elapsed().as_millis() as f32;
         let simulation_delta = simulation_elapsed_ms - state.simulation_last_update_ms;
@@ -209,6 +242,47 @@ fn main() {
         }
 
         state.window.set_light(Light::Absolute(Point3::new(100.0, 100.0, 100.0)));
+
+        // Draw HUD elements
+        draw_planar_cursor(&mut state, &cursor_position);
+        // draw_hud_text(&mut state, 
+        //     &format!("Cursor(World): {},{},{}", cursor_position_world.x, cursor_position_world.y, cursor_position_world.z),
+        //     &Point2::new(0.0,0.0), 50.0);
+
         state.window.render_with_camera(&mut camera); 
     }
+}
+
+fn draw_planar_cursor( state: &mut AppState, cursor_position : &na::Point2<f32> ) {
+    const CROSS_SIZE: f32 = 10.0;
+    let cursor_colour = na::Point3::new(1.0, 0.5, 1.0);
+    let up = na::Vector2::new(CROSS_SIZE, 0.0);
+
+    // TODO: HAX!
+    // draw_planar line seems to take pixel offsets from screen center
+    // cursor_position is hacked from the planar_camera (despite not using it), so
+    // here we transform from -1.0-1.0 -> what draw_planar_line needs
+    let cursor_planar_pos = na::Point2::new(
+        cursor_position.x * (state.window.size()[0] / 2) as f32,
+        cursor_position.y * (state.window.size()[1] / 2) as f32,
+    );
+
+    // let cursor_position = na::Point2::new(100.0, 100.0);
+    // println!("Draw cursor: {:?}", cursor_position);
+    state.window.draw_planar_line(&(cursor_planar_pos - up), &(cursor_planar_pos + up), &cursor_colour);
+
+    let right = na::Vector2::new(0.0, CROSS_SIZE);
+    state.window.draw_planar_line(&(cursor_planar_pos - right), &(cursor_planar_pos + right), &cursor_colour);
+}
+
+fn draw_hud_text( state: &mut AppState, text : &str, position : &na::Point2<f32>, size: f32 ) {
+    let font = Font::default();
+    // Text coordinates are in pixels, from top-left
+    state.window.draw_text(
+        &text,
+        &position,
+        size,
+        &font,
+        &Point3::new(1.0, 1.0, 1.0),
+    );
 }
