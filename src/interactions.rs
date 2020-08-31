cf303badd7edd8cd601fe428b12727b1ad7e8809
Gareth Francis <gfrancis.dev@gmail.com>
2020-08-31 09:35:01 +0100

    Interaction improvements for level creation

--- src/interactions.rs
+++ src/interactions.rs
@@ -4,7 +4,7 @@ extern crate nalgebra as na;
 
 use crate::engine::*;
 
-use na::{Point2, Vector3};
+use na::{Point2, Point3, Vector3, Translation3, Isometry3, UnitQuaternion};
 
 use ncollide3d::shape::{Cuboid};
 use ncollide3d::query::{Ray, RayCast};
@@ -12,7 +12,10 @@ use ncollide3d::pipeline::object::CollisionGroups;
 
 use kiss3d::event::{Key, MouseButton, Modifiers};
 use kiss3d::camera::{Camera};
+use kiss3d::scene::SceneNode;
 use kiss3d::planar_camera::*;
+
+use std::path::Path;
 
 pub trait Interaction {
     fn on_key_down( &mut self, state : &mut AppState, k : &Key, modif : &Modifiers );
@@ -33,6 +36,7 @@ pub struct EditorModeInteraction {
     pub primitive_name : String,
     cursor_ray : Ray<f32>,
     cursor_position : Point2<f32>,
+    cursor_position_world : Vector3<f32>,
     primitive_rotation : na::Vector3<f32>,
     primitive_rotation_delta : f32,
     primitive_spawn_height : f32,
@@ -42,6 +46,7 @@ pub struct EditorModeInteraction {
     primitive_placement_mode : EditorPlacementMode,
     primitive_placement_static : bool,
     mouse_button1_pressed : bool,
+    render_preview : Option<SceneNode>,
 }
 impl EditorModeInteraction {
     pub fn new(ground_collision_cuboid : Cuboid<f32>, primitive_name : &str) -> Self {
@@ -50,6 +55,7 @@ impl EditorModeInteraction {
             ground_collision_cuboid,
             cursor_ray : Ray::new(na::Point3::new(0.0,0.0,0.0), na::Vector3::new(0.0,0.0,0.0)),
             cursor_position : na::Point2::<f32>::new(0.0,0.0),
+            cursor_position_world : na::Vector3::<f32>::new(0.0,0.0,0.0),
             primitive_name : String::from(primitive_name),
             primitive_rotation : na::Vector3::new(0.0,0.0,0.0),
             primitive_rotation_delta : 15.0_f64.to_radians() as f32,
@@ -60,6 +66,7 @@ impl EditorModeInteraction {
             primitive_placement_mode : EditorPlacementMode::Singular,
             primitive_placement_static : false,
             mouse_button1_pressed : false,
+            render_preview : None,
         }
     }
 }
@@ -84,22 +91,29 @@ impl Interaction for EditorModeInteraction {
                     }
                 }
             },
-            Key::Q => self.primitive_rotation = Vector3::new(0.0,0.0,0.0),
-            Key::E => {
-                if self.primitive_spawn_height > 1.0 {
+            Key::Q => {
+                self.primitive_spawn_height -= 0.5;
+                if self.primitive_spawn_height < 0.5 {
                     self.primitive_spawn_height = 0.5;
-                } else {
-                    self.primitive_spawn_height = 30.0;
+                }
+                if let Some(intersection_point) = self.get_primitive_spawn_position(state) {
+                    self.cursor_position_world = intersection_point;
+                }
+            }
+            Key::E => {
+                self.primitive_spawn_height += 0.5;
+                if let Some(intersection_point) = self.get_primitive_spawn_position(state) {
+                    self.cursor_position_world = intersection_point;
                 }
             },
             Key::A => self.primitive_rotation.y += self.primitive_rotation_delta,
             Key::D => self.primitive_rotation.y -= self.primitive_rotation_delta,
-            Key::W => self.primitive_rotation.x -= self.primitive_rotation_delta,
+            // Key::W => self.primitive_rotation.x -= self.primitive_rotation_delta,
             Key::S => {
                 if modif.contains(Modifiers::Control) {
                     save_level_definition(&state.level_definition, &state.level_file);
                 } else {
-                    self.primitive_rotation.x += self.primitive_rotation_delta;
+               //     self.primitive_rotation.x += self.primitive_rotation_delta;
                 }
             },
             Key::R => self.primitive_spawn_spacing *= 1.1,
@@ -142,7 +156,7 @@ impl Interaction for EditorModeInteraction {
             self.mouse_button1_pressed = false;
         }
     }
-    fn on_mouse_move( &mut self, state : &mut AppState, x : f32, y : f32, _modif : &Modifiers ) {
+    fn on_mouse_move( &mut self, state : &mut AppState, x : f32, y : f32, modif : &Modifiers ) {
         let window_size = na::Vector2::new(state.window.size()[0] as f32, state.window.size()[1] as f32);
         let cursor_position_projected = na::Point2::new(x as f32, y as f32);
     
@@ -156,6 +170,8 @@ impl Interaction for EditorModeInteraction {
         
         // Find the intersection between cursor ray and ground, then spawn something
         if let Some(intersection_point) = self.get_primitive_spawn_position(state) {
+            self.cursor_position_world = intersection_point;
+
             match self.primitive_placement_mode {
                 EditorPlacementMode::Instanced => {
                     if (intersection_point - self.primitve_last_spawn_pos).magnitude() > self.primitive_spawn_spacing {
@@ -178,30 +194,23 @@ impl Interaction for EditorModeInteraction {
     }
 
     fn render( &mut self, state : &mut AppState ) {
+        // Disabled cause it's broken:  W/S: Rotate Primitive X
         let control_text = format!(
 "Controls:
     Right Mouse: Rotate Camera
     Middle Mouse: Translate Camera
     Left drag: Spawn object: {}
     A/D: Rotate Primitive Y
-    W/S: Rotate Primitive X
-    Q  : Reset Primitive Rotation
-    E  : Toggle spawn height
-    R  : Increase spacing
-    F  : Decrease spacing
-    C  : Toggle auto-rotate
-    X  : Cycle placement mode
-    Z  : Static/Dynamic primitive
     Ctrl+S : Save level ({})
 
-Number of Primitives   : {},
-Auto-Rotate Active     : {},
-Placement Mode         : {:?},
-Spawn Static Primtive  : {},
-Primitive Rotation     : {}°, {}°, {}°
-Primitive Spawn Height : {}
-Primitive Spacing      : {}
-Primitive auto-rotate  : {}",
+Number of Primitives         : {},
+Auto-Rotate Active           : {},
+X   : Placement Mode         : {:?},
+Z   : Static Primitives      : {},
+      Primitive Rotation     : {}°, {}°, {}°
+Q/E : Primitive Spawn Height : {}
+R/F : Primitive Spacing      : {}
+C   : Primitive auto-rotate  : {}",
         self.primitive_name,
         state.level_definition.name,
         state.physics_entities.len(),
@@ -230,32 +239,77 @@ Primitive auto-rotate  : {}",
             self.cursor_position.y * (state.window.size()[1] / 2) as f32,
         );
 
-        state.window.draw_planar_line(&(cursor_planar_pos - up), &(cursor_planar_pos + up), &cursor_colour);
+        // state.window.draw_planar_line(&(cursor_planar_pos - up), &(cursor_planar_pos + up), &cursor_colour);
 
-        let right = na::Vector2::new(0.0, CROSS_SIZE);
-        state.window.draw_planar_line(&(cursor_planar_pos - right), &(cursor_planar_pos + right), &cursor_colour);
+        // let right = na::Vector2::new(0.0, CROSS_SIZE);
+        // state.window.draw_planar_line(&(cursor_planar_pos - right), &(cursor_planar_pos + right), &cursor_colour);
+
+
+        // TODO: Inefficient
+        // Remove any old preview
+        if let Some(x) = &mut self.render_preview {
+            state.window.remove_node(x);
+            self.render_preview = None;
+        }
+
+        // Make a new preview
+        if let Some(prim) = state.primitives_library.get_mut(&self.primitive_name) {
+            let prim_scale = Vector3::from(prim.scale);
+            let mut gfx = state.window.add_obj(
+                Path::new(&format!("{}/{}", state.assets_path, prim.path_obj)),
+                Path::new(&format!("{}/{}", state.assets_path, prim.path_mtl)),
+                prim_scale,
+            );
+            gfx.set_color(0.8, 0.1, 0.8);
+            gfx.set_points_size(4.0);
+            gfx.set_lines_width(4.0);
+            gfx.set_surface_rendering_activation(false);
+
+            let trans = Translation3::new(
+                self.cursor_position_world.x,
+                self.cursor_position_world.y,
+                self.cursor_position_world.z,
+            );
+            let rot = UnitQuaternion::from_euler_angles(self.primitive_rotation.x, self.primitive_rotation.y, self.primitive_rotation.z);
+            let iso = Isometry3::from_parts(trans, rot);
+
+            gfx.set_local_transformation(iso);
+            
+            self.render_preview = Some(gfx);
+        }
+
+        // state.window.set_point_size(10.0);
+        // for p in &self.render_preview_points {
+        //     state.window.draw_point(&p, &Point3::new(1.0, 0.0, 1.0))
+        // }
     }
 }
 impl EditorModeInteraction {
     fn get_primitive_spawn_position(&mut self, state : &mut AppState) -> Option<Vector3<f32>> {
-        // TODO: This is a copy of code below, de-dupe it
         // Find the intersection between cursor ray and an object, then spawn something
         // Ideally the user clicked on the top of an object, but if it's the side we'll spawn anyway
-        let groups = CollisionGroups::new();
-        let ray_interferences = state.geometrical_world.interferences_with_ray(&state.colliders, 
-            &self.cursor_ray, 10000.0, &groups);
+
+        let intersect_all = false;
         let mut toi = 10000.0;
-        for (_, b, inter) in ray_interferences {
-            if !b.query_type().is_proximity_query() && inter.toi < toi {
-                toi = inter.toi;
+        if intersect_all {
+            let groups = CollisionGroups::new();
+            let ray_interferences = state.geometrical_world.interferences_with_ray(&state.colliders, 
+                &self.cursor_ray, 10000.0, &groups);
+            
+            for (_, b, inter) in ray_interferences {
+                if !b.query_type().is_proximity_query() && inter.toi < toi {
+                    toi = inter.toi;
+                }
             }
         }
+        else {
+          toi = 0.0;
+          if let Some(x) = self.ground_collision_cuboid.toi_with_ray(&na::Isometry3::identity(), &self.cursor_ray, 10000.0, true) {
+            toi = x;
+          }
+        }
 
-        //if let Some(toi) = self.ground_collision_cuboid.toi_with_ray(&na::Isometry3::identity(), &self.cursor_ray, 10000.0, true) {
         if toi > 0.0 {
-
-
-
             let intersection_point = self.cursor_ray.origin + self.cursor_ray.dir * toi;
             let intersection_point = na::Vector3::new(
                 intersection_point.x,
